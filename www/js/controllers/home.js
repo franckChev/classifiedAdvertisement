7c8c5d66b42e4a4182fee247d32dd1c15f6ed80a
franckChev <franck.chev@hotmail.fr>
2014-11-14 17:31:20 +0100

    -
    -

--- www/js/controllers/home.js
+++ www/js/controllers/home.js
@@ -1,10 +1,12 @@
 app.controller('HomeCtrl', function($scope, $location, AdvertisementsFactory, $ionicLoading) {
     $scope.adimgpath = "http://fontromeu-application.com//images/com_adsmanager/ads/";
+    $scope.mode = "list";
     if (localStorage["advertisements"] != undefined) {
         $scope.advertisements = JSON.parse(localStorage["advertisements"]);
     } else {
         $scope.advertisements = AdvertisementsFactory.getAdvertisements().then(function(advertisements) {
             $scope.advertisements = advertisements;
+            console.log($scope.advertisements);
         }, function(msg) {
             alert(msg);
         });
